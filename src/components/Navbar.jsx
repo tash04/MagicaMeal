358e560fa42ce4dedac3af636045bef72f57341f
Ashita Tiwari <119289726+tash04@users.noreply.github.com>
2023-04-17 07:24:04 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
-import Badge from "@material-ui/core/Badge";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useCart } from './ContextReducer';
 import Modal from '../Modal';
 import Cartscreen from '../screens/Cartscreen';
@@ -50,19 +48,18 @@ export default function Navbar() {
                                 <Link className="btn bg-white text-success mx-1 " to="/login">Login</Link>
                                 <Link className="btn bg-white text-success mx-1" to="/signup">Signup</Link>
                                 <Link className="btn bg-white text-success mx-1" to='/cart'>Cart {cartstate.cartItems.length}</Link>
-             
+                           </form> :
 
-                            </form> :
-                            <div>
+                             <div>
 
-                                <div className="btn bg-white text-success mx-2 " onClick={loadCart}>
-                                    <Badge color="secondary" badgeContent={items.length} >
-                                        <ShoppingCartIcon />
-                                    </Badge>
-                                    Cart
-                                </div>
+//                                 <div className="btn bg-white text-success mx-2 " onClick={loadCart}>
+//                                     <Badge color="secondary" badgeContent={items.length} >
+//                                         <ShoppingCartIcon />
+//                                     </Badge>
+//                                     Cart
+//                                 </div>
 
-                                {cartView ? <Modal onClose={() => setCartView(false)}><Cartscreen></Cartscreen></Modal> : ""}
+//                                 {cartView ? <Modal onClose={() => setCartView(false)}><Cartscreen></Cartscreen></Modal> : ""}
 
                                 <button onClick={handleLogout} className="btn bg-white text-success" >Logout</button></div>}
                     </div>
@@ -73,39 +70,3 @@ export default function Navbar() {
 }
 
 
-// const Navbar = () => {
-//   return (
-//     <div>
-//         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-//   <div className="container-fluid">
-//     <Link className="navbar-brand fs-1 fst-italic" to="/">MagicaMeal</Link>
-//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//       <span className="navbar-toggler-icon"></span>
-//     </button>
-
-//     <div className="collapse navbar-collapse" id="navbarNav">
-
-//       <ul className="navbar-nav">
-//         <li className="nav-item">
-//           <Link className="nav-link" aria-current="page" to="/">Home</Link>   
-//           </li>
-
-//         <li className="nav-item" >
-//         <Link className="nav-link" to="/login">Login</Link>   
-//         </li>
-
-//         <li className="nav-item" >
-//         <Link className="nav-link" to="/createuser">SignUp</Link>   
-//         </li>
-
-//         <li className="nav-item" ></li>
-        
-//       </ul>
-//     </div>
-//   </div>
-// </nav>
-//     </div>
-//   )
-// }
-
-// export default Navbar;
